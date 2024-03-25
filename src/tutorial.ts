266920457da54f575a33f258e58677ce4d5d0c9c
Adeleke Hammed <lekhad19@gmail.com>
2024-03-25 16:20:52 +0100

    first commit

---
+++ src/tutorial.ts
@@ -0,0 +1,41 @@
+// console.log('typescript tutorial');
+
+// interface someValues {
+//     name: string; 
+//     id: number;
+// }
+
+// let someObj:someValues = {
+//     name : 'random', 
+//     id: 123, 
+// }; 
+
+// console.log(someObj); 
+
+let awesomeName: string = 'shakeAndBake'; 
+awesomeName = 'something';
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+let amount: number = 20; 
+amount = 12  - 1;
+
+let isAwesome:boolean = true; 
+isAwesome = false; 
+// isAwesome = 'shakeAndBake'; 
+
+////////////////////////////////////////challenge /////////////////////////
+let greeting: string = "Hello, Typescript"; 
+greeting = greeting.toUpperCase(); 
+
+let age: number = 25; 
+age = age + 2; 
+
+let isAdult: boolean = age >=18; 
+isAdult = !isAdult; 
+console.log(isAdult); 
+
+// greeting = 10; 
+// age = 'thirty'; 
+// isAdult = '25';
+console.log(greeting, age, isAdult); 
