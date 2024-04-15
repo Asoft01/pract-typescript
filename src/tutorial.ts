a85c514d29a0f64597054975fe1217e1986229bc
Adeleke Hammed <lekhad19@gmail.com>
2024-04-15 18:11:24 +0100

    objects

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -68,29 +68,56 @@
 // foundBook = 10; 
 // console.log(foundBook); 
 ////////////////////////////////////////////////////////////////////////////
-// const books = ['1984', 'Brave New World', 'Lookman 451']; 
-// let foundBook: string | undefined; 
+const books = ['1984', 'Brave New World', 'Lookman 451']; 
+let foundBook: string | undefined; 
 
-// for(let book of books) {
-//     if(book === '1984') {
-//         foundBook = book;
+for(let book of books) {
+    if(book === '1984') {
+        foundBook = book;
 
-//         foundBook = foundBook.toUpperCase(); 
-//         break;
-//     }
-// }
-
-// console.log(foundBook?.length);
+        foundBook = foundBook.toUpperCase(); 
+        break;
+    }
+}
+console.log(foundBook?.length);
 
 ////////////////////////////////////////////////////////////////////////////
 // Challenge 
 // Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
 // Create a variable discount of type number | string and assign it the value 20. Then, try to assign it the value '20%'.
 
-let orderStatus:  'processing' | 'shipped' | 'delivered' = 'processing'; 
-orderStatus = 'shipped' 
-orderStatus = 'delivered';
+// let orderStatus:  'processing' | 'shipped' | 'delivered' = 'processing'; 
+// orderStatus = 'shipped' 
+// orderStatus = 'delivered';
 
-let discount: number | string = 20;
-discount = '20%';
-console.log(discount);
+// let discount: number | string = 20;
+// discount = '20%';
+// console.log(discount);
+
+////////////////////////////////////////////////////////////////////////////////
+// let prices: number[] = [100, 75, 42]; 
+// // prices.push('hello'); 
+
+// let fruit: string[] = ['apple', 'orange']; 
+
+// // let randomValues:[] = ['hello'];  // it will complain
+// let emptyValue = []; 
+//////////////////////////////////////////////////////////////////////////////
+// let names = ['peter', 'susan', 1];
+// let array:(string | boolean)[] = ['apple', true, 'orange', false];
+
+//////////////////////////////////////////////////////////////////////////////
+// let temperatures:number[] = [20, 25, 30]; 
+// // console.log(temperatures); 
+// // temperatures.push('hot');
+// let colors: string[] = ['red', 'green', 'blue'];
+// // colors.push(true);
+
+// let mixedArrays:(number | string)[] = [1, 'two', 3];
+// // console.log(mixedArrays);
+
+///////////////////////////////// Objects in Typescripts ///////////////////////////
+let car:{brand: string, year: number } = {brand: 'toyota', year: 2020};
+
+car.brand = 'ford';
+// car.color
